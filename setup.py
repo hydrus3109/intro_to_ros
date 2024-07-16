192f39a91fe367ea5660dfd16593ff5fa35f3c1d
Aidan <agao3019@gmail.com>
2024-07-16 14:28:46 -0400

    physics sub

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'console_scripts': ['publisher = intro_to_ros.publisher:main',
                             'subscriber = intro_to_ros.subscriber:main',
                             'subscriber2 = intro_to_ros.bluerov2_sensors:main'
+                            'physicssubscriber = intro_to_ros.physics_sim:main'
         ],
     },
 )
